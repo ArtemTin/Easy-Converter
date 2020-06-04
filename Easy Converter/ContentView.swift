be21996e43bdab37307e8f948ee0a2b6e111c800
Artem Tikhonenko <s21d_tihonenko@179.ru>
2020-06-04 03:28:58 +0300

    Initial Commit

---
+++ Easy Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Easy Converter
+//
+//  Created by Артём Тихоненко on 04.06.2020.
+//  Copyright © 2020 Артём Тихоненко. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
